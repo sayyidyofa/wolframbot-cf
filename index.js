28eae74f7ceba1c0b5f3e7a6d6c33b765e154ad3
Sayyid Yofa <sayyidyofa@gmail.com>
2021-10-25 13:06:50 +0700

    waitUntil shenanigans

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    handleRequest(event.request).catch(
-      (err) => new Response(err.stack, { status: 500 })
-    )
-  );
-});
+function panic(reason) {
+  console.error(reason);
+  throw reason;
+}
 
-const helpMessage = "\nList of Commands:" +
+const helpMessage = "List of Commands:" +
   "\n/solve: Find the solution of a quadratic equation, input example:" +
   "\n/solve x^2 - y^2 + 1 = 0";
 
@@ -67,7 +64,7 @@ async function callWolfram(eqString) {
   return {graph: result.pop(), solution: result.join(", ")};
 }
 
-async function handleRequest(request) {
+async function handleRequest({ request, wait }) {
   const { pathname } = new URL(request.url);
   const lePath = `/${ENDPOINT}`;
 
@@ -81,36 +78,50 @@ async function handleRequest(request) {
       .filter(e => e.source.type === "user");
 
     for (const lineDM of lineDMs) {
-      if (lineDM.message.text === "/help") Promise.resolve(reply([
-        {type: "text", text: helpMessage}
-      ], lineDM.replyToken)).then(console.log);
+      if (lineDM.message.text === "/help") wait((async () => {
+        await reply([
+          {type: "text", text: helpMessage}
+        ], lineDM.replyToken);
+      })().catch(panic));
       else {
         if (lineDM.message.text.startsWith("/solve")) {
-          Promise.resolve(reply([
-            {type: "text", text: waitingMessage[Math.floor(Math.random()*waitingMessage.length)]}
-          ], lineDM.replyToken)).then(console.log);
+          wait((async () => {
+            await reply([
+              {type: "text", text: waitingMessage[Math.floor(Math.random()*waitingMessage.length)]}
+            ], lineDM.replyToken);
 
-          callWolfram(lineDM.message.text.slice(7))
-            .then(resultAndGraph => {
-              const {graph, solution} = resultAndGraph;
-              Promise.resolve(dm([
-                {type: "text", text: `Real solution: ${solution}`},
-                {type: "text", text: "Graphical plot:"},
-                {type: "image", originalContentUrl: graph, previewImageUrl: graph}
-              ], lineDM.source.userId)).then(console.log);
-            }).catch(reason => {
-            Promise.resolve(dm([
+            const {graph, solution} = await callWolfram(lineDM.message.text.slice(7));
+
+            await dm([
+              {type: "text", text: `Real solution: ${solution}`},
+              {type: "text", text: "Graphical plot:"},
+              {type: "image", originalContentUrl: graph, previewImageUrl: graph}
+            ], lineDM.source.userId);
+
+          })().catch(reason => {
+            dm([
               {type: "text", text: `Math expression is not understandable. Details: ${reason}`}
-            ], lineDM.source.userId)).then(console.log);
-          });
-        } else Promise.resolve(reply([
-          {type: "text", text: "Type /help for help"}
-        ], lineDM.replyToken)).then(console.log);
+            ], lineDM.source.userId).catch();
+            panic(reason);
+          }));
+
+        } else wait((async () => {
+          await reply([
+            {type: "text", text: "Type /help for help"}
+          ], lineDM.replyToken);
+        })().catch(panic));
       }
     }
-
     return new Response("Ok");
   }
-
   return new Response("No");
 }
+
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest({
+      request: event.request,
+      wait: event.waitUntil.bind(event)
+    }).catch(panic)
+  );
+});
